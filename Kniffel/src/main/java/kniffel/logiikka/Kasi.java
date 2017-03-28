187c0e4b3e78a2f2848cf1f29f9c4f374869dbad
Sädekoski Niklas <niklas.sadekoski@helsinki.fi>
2017-03-28 16:06:55 +0300

    uusia luokkia

---
+++ Kniffel/src/main/java/kniffel/logiikka/Kasi.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kniffel.logiikka;
+
+import java.util.List;
+import kniffel.nopat.Noppa;
+
+/**
+ *
+ * @author niklasad
+ */
+public class Kasi {
+    
+    private List<Noppa> nopat;
+    
+    public Kasi(List<Noppa> nopat){
+        this.nopat = nopat;
+    }
+    
+    public void valitseTulos(Tulos tulos){
+        
+    }
+    
+}
